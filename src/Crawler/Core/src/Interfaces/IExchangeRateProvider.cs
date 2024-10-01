9cffc241fc9793511b487288f62c65d0d09c3757
Timur Vafin <51379496+Redplcs@users.noreply.github.com>
2024-10-01 17:18:50 +0500

    Add exchange rate provider interface and value object

---
+++ src/Crawler/Core/src/Interfaces/IExchangeRateProvider.cs
@@ -0,0 +1,8 @@
+﻿using ExchangeRateTracker.Crawler.Core.ValueObjects;
+
+namespace ExchangeRateTracker.Crawler.Core.Interfaces;
+
+public interface IExchangeRateProvider
+{
+	IReadOnlyCollection<ExchangeRate> GetExchangeRates(DateOnly date);
+}
---
+++ src/Crawler/Core/src/ValueObjects/ExchangeRate.cs
@@ -0,0 +1,5 @@
+﻿namespace ExchangeRateTracker.Crawler.Core.ValueObjects;
+
+public record ExchangeRate
+{
+}
